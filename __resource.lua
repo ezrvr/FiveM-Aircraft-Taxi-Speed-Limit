ac7d7e8ffa3ccca64bb26a3c6e65183ea8baf44d
EZRVR <169106988+ezrvr@users.noreply.github.com>
2024-05-07 00:48:23 +0100

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'Propeller FS - ezvr'
+description 'Applies Aircraft Taxi Speed Limit on activation (15mph)'
+version '1.0.0'
+
+client_script 'script.lua'
