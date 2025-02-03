0e183a906fa1b482748320f84ea8d1fe0e4090cf
EZRVR <169106988+ezrvr@users.noreply.github.com>
2025-02-03 15:28:01 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,18 +1,26 @@
--- CREATED BY EZRVR --
--- FIVEM TAXI SPEED LIMIT --
+-- CREATED BY EZRD DEV --
+-- V2 / FINAL -- 
 
 local taxiSpeedLimitActive = false
 local taxiSpeedLimit = 15.0 -- Taxi speed limit in meters per second (15 mph)
 
 -- Function to toggle taxi speed limit on/off
 function ToggleTaxiSpeedLimit()
-    taxiSpeedLimitActive = not taxiSpeedLimitActive
+    local playerPed = GetPlayerPed(-1)
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+    local vehicleClass = GetVehicleClass(vehicle)
+    
+    if vehicleClass == 16 then -- Vehicle class 16 corresponds to planes
+        taxiSpeedLimitActive = not taxiSpeedLimitActive
 
-    if taxiSpeedLimitActive then
-        print("Taxi speed limit activated (15 mph)")
+        if taxiSpeedLimitActive then
+            print("Taxi speed limit activated (15 mph)")
+        else
+            print("Taxi speed limit deactivated")
+            ResetTaxiSpeedLimit() -- Reset the taxi speed limit
+        end
     else
-        print("Taxi speed limit deactivated")
-        ResetTaxiSpeedLimit() -- Reset the taxi speed limit
+        print("Taxi speed limit unavailable")
     end
 end
 
@@ -30,3 +38,58 @@ function LimitTaxiSpeed()
     if altitude <= 5 then
         local playerPed = GetPlayerPed(-1)
         local vehicle = GetVehiclePedIsIn(playerPed, false)
+        local currentSpeed = GetEntitySpeed(vehicle)
+
+        if taxiSpeedLimitActive then
+            if currentSpeed > (taxiSpeedLimit * 0.447) then -- Convert mph to m/s
+                SetEntityMaxSpeed(vehicle, taxiSpeedLimit * 0.447) -- Set max speed in m/s
+            end
+        else
+            ResetTaxiSpeedLimit() -- Reset the taxi speed limit if not active
+        end
+    end
+end
+
+-- Function to draw taxi limit status
+function DrawTaxiLimitStatus()
+    local playerPed = GetPlayerPed(-1)
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+    local vehicleClass = GetVehicleClass(vehicle)
+    local altitude = GetEntityHeightAboveGround(vehicle)
+    local status = ""
+    
+    if vehicleClass ~= 16 then
+        status = "~c~UNAVAILABLE"
+    elseif altitude > 5 then
+        status = "NOT AVAILABLE"
+    else
+        status = taxiSpeedLimitActive and "~g~ON" or "~r~OFF"
+    end
+    
+    local text = "Taxi Limit: " .. status
+
+    SetTextFont(4) -- Font used for the other resource
+    SetTextProportional(0)
+    SetTextScale(0.45, 0.45)
+    SetTextColour(255, 255, 255, 255, 50)
+    SetTextDropShadow(0, 0, 0, 0, 255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(0.175, 0.905)
+end
+
+-- Bind key to toggle taxi speed limit
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(0, 131) then -- Key Control for Taxi Limit (example: "K" key)
+            ToggleTaxiSpeedLimit()
+        end
+
+        LimitTaxiSpeed()
+        DrawTaxiLimitStatus()
+    end
+end)
